1ff554f7af02c0d2646db6bd379f0a488ce0828b
egargo <clintegargo@protonmail.ch>
2023-03-07 03:39:03 +0800

    Updated: added faculty route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import app from './lib/express';
+import * as faculty from './api/v1/users/faculty';
 import * as student from './api/v1/users/student';
 
+faculty;
 student;
 
 export const server = app.listen(process.env.EXPRESS_PORT, () => {
