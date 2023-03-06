6263103dd0d54789552b3d485a5ef93230b0c877
egargo <clintegargo@protonmail.ch>
2023-03-07 04:15:35 +0800

    Updated: added announcements route

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ dotenv.config();
 import app from './lib/express';
 import * as faculty from './api/v1/users/faculty';
 import * as student from './api/v1/users/student';
+import * as announcements from './api/v1/announcements';
 
 faculty;
 student;
+announcements;
 
 export const server = app.listen(process.env.EXPRESS_PORT, () => {
   console.log(`Server ready at: ${process.env.EXPRESS_PORT}`);
