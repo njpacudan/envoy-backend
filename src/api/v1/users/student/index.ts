0d3757bd50565f0c23cd793735dad71841438093
egargo <clintegargo@protonmail.ch>
2023-03-07 03:29:52 +0800

    Updated: changed get to list

--- src/api/v1/users/student/index.ts
+++ src/api/v1/users/student/index.ts
@@ -5,7 +5,7 @@ import prisma from '../../../../lib/prisma';
 
 const URL = `${process.env.API_BASE_URL}/users/student`;
 
-export const get = app.get(URL, async (_req: any, res: any) => {
+export const list = app.get(URL, async (_req: any, res: any) => {
   let students = await prisma.users_student.findMany();
 
   res.status(200).json(students);
