f77e42cb3882c78ee361ada455ad8724e19fff5c
Clint Egargo <clintegargo@protonmail.ch>
2023-03-16 12:51:20 +0800

    refactor: changed announcements/index.ts to announcements.route.ts

---
+++ src/routes/announcements.route.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { app } from '@lib/express';
+import news from '@controllers/announcements';
+
+const URL = `${process.env.API_BASE_URL}/announcements`;
+
+export const list = app.get(URL, news.list)
+
+export const announcement = app.get(`${URL}/info`, news.info);
+
+export const news_course = app.get(`${URL}/course`, news.course);
+
+export const post = app.post(`${URL}/post`, news.post);
