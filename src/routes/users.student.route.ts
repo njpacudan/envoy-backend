fc64b47ef436a6209e52aa0df8c83fcf8b822323
Clint Egargo <clintegargo@protonmail.ch>
2023-03-16 14:33:52 +0800

    feat: added /create endpoint

--- src/routes/users.student.route.ts
+++ src/routes/users.student.route.ts
@@ -2,5 +2,6 @@ import { router } from '@lib/express';
 import studentController from '@controllers/users.student.controllers';
 
 router.get('/', studentController.list);
+router.post('/create', studentController.create);
 
 export default router;
