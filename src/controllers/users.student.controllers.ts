6b89460704f59c73bf5c15fb19d1c2f5abd6796c
Clint Egargo <clintegargo@protonmail.ch>
2023-03-23 16:27:41 +0800

    feat: removed create

--- src/controllers/users.student.controllers.ts
+++ src/controllers/users.student.controllers.ts
@@ -16,31 +16,6 @@ const studentController = {
         res.status(200).json(student);
     },
 
-    create: async (req: any, res: any) => {
-        // Check if email is already used.
-        const validateEmail = await prisma.users_student.findUnique({
-            where: {
-                email: req.body.email,
-            }
-        });
-
-        if(!validateEmail) {
-            const student = await prisma.users_student.create({
-                data: {
-                    // Name, email, course, passsword
-                    ...req.body,
-                    password: await bcrypt.hash(req.body.password, 12),
-                    date_registered: new Date(),
-                    last_login: new Date(),
-                    status: 'Active',
-                },
-            });
-            res.status(201).json(student);
-        } else {
-            return res.status(409).json('Email is already used.');
-        }
-    },
-
     disable: async (req: any, res: any) => {
         await prisma.users_student.update({
             where: {
