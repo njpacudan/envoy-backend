d33d798eb62c246a9c8d1ec7f9e2f333fe5f3be8
Clint Egargo <clintegargo@protonmail.ch>
2023-03-23 16:30:35 +0800

    feat: removed routes

--- src/routes/users.student.route.ts
+++ src/routes/users.student.route.ts
@@ -2,6 +2,5 @@ import { router } from '@lib/express';
 import studentController from '@controllers/users.student.controllers';
 
 router.get('/', studentController.list);
-router.post('/create', studentController.create);
 
 export default router;
